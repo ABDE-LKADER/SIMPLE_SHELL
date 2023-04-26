01b7f88a35b3ba7c7593ab536b1fea8fb9354fc0
Hajar El Hjouji <122790128+Its-joujou@users.noreply.github.com>
2023-04-27 00:13:44 +0200

    Update parse_command.c

--- parse_command.c
+++ parse_command.c
@@ -1,24 +1,86 @@
 #include "shell.h"
 
 /**
- * parse_command -> parses a command
+ * is_cmd - determines if a file is an executable command
+ * @info: the info struct
+ * @path: path to the file
  *
- * @command: Command
- *
- * Return: Arg Array
-*/
-
-char **parse_command(char *command)
+ * Return: 1 if true, 0 otherwise
+ */
+int is_cmd(info_t *info, char *path)
 {
-	char **args = malloc(sizeof(char *) * BUFFER_SIZE);
-	char *arg = strtok(command, " \t\n");
-	int i = 0;
+	struct stat st;
 
-	while (arg != NULL)
+	(void)info;
+	if (!path || stat(path, &st))
+		return (0);
+
+	if (st.st_mode & S_IFREG)
 	{
-		args[i++] = arg;
-		arg = strtok(NULL, " \t\n");
+		return (1);
 	}
-	args[i] = NULL;
-	return (args);
+	return (0);
+}
+
+/**
+ * dup_chars - duplicates characters
+ * @pathstr: the PATH string
+ * @start: starting index
+ * @stop: stopping index
+ *
+ * Return: pointer to new buffer
+ */
+char *dup_chars(char *pathstr, int start, int stop)
+{
+	static char buf[1024];
+	int i = 0, k = 0;
+
+	for (k = 0, i = start; i < stop; i++)
+		if (pathstr[i] != ':')
+			buf[k++] = pathstr[i];
+	buf[k] = 0;
+	return (buf);
+}
+
+/**
+ * find_path - finds this cmd in the PATH string
+ * @info: the info struct
+ * @pathstr: the PATH string
+ * @cmd: the cmd to find
+ *
+ * Return: full path of cmd if found or NULL
+ */
+char *find_path(info_t *info, char *pathstr, char *cmd)
+{
+	int i = 0, curr_pos = 0;
+	char *path;
+
+	if (!pathstr)
+		return (NULL);
+	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
+	{
+		if (is_cmd(info, cmd))
+			return (cmd);
+	}
+	while (1)
+	{
+		if (!pathstr[i] || pathstr[i] == ':')
+		{
+			path = dup_chars(pathstr, curr_pos, i);
+			if (!*path)
+				_strcat(path, cmd);
+			else
+			{
+				_strcat(path, "/");
+				_strcat(path, cmd);
+			}
+			if (is_cmd(info, path))
+				return (path);
+			if (!pathstr[i])
+				break;
+			curr_pos = i;
+		}
+		i++;
+	}
+	return (NULL);
 }
