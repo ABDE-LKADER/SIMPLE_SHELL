1e711b938f7260fa95dadbcb8718601d9eadc6b3
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-04-26 23:37:52 +0100

    New One

--- shell.h
+++ shell.h
@@ -4,42 +4,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include <sys/wait.h>
-#include <sys/types.h>
+#include <sys/stat.h>
 #include <signal.h>
-#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <dirent.h>
+
+#define PROMPT "$ "
+#define DELIM " \n\t\r\a"
+#define MAXARGS 10
+#define MAXPATH 1024
 
 extern char **environ;
 
-int _strcmp(char *s1, char *s2);
-char *_strdup(char *str);
-int _strlen(char *str);
-char *_strcat(char *dest, char *src);
-int _strncmp(char *s1, char *s2, int n);
-char *_strchr(char *s, char c);
-
-void free_array(char **arr);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+/* function prototypes */
 
 char *read_line(void);
-char **split_line(char *line, char *delimiter);
-void execute(char **args, char **env);
-int built_in(char **args, char **env);
-void change_dir(char **args, char **env);
-void print_env(char **env);
-void set_env(char **args, char **env);
-void unset_env(char **args, char **env);
-void print_alias(char **alias);
-void set_alias(char **alias, char **args);
-void alias(char **args, char **alias);
-void replace_vars(char **args, int *exit_code, pid_t pid);
-
-char *get_env(char *name);
-char *_itoa(pid_t pid);
-char *_getenv(char *name, char **env);
-void _setenv_var(char *name, char *value, char **env);
-
-void sigint_handler(int sig);
+char **split_line(char *line);
+int execute(char **args);
+int launch(char **args);
+int cd(char **args);
+int setenv_cmd(char **args);
+int unsetenv_cmd(char **args);
+int printenv(char **args);
+int exit_cmd(char **args);
+char *replace_vars(char *line);
+int handle_operators(char **args, int result);
+int handle_semicolon(char **args);
+int handle_alias(char **args);
+int shell(char **args);
+void handler(int signum);
 
 #endif
