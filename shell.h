a635b0ce7d783b6818245facf5d787d5de1d5e3c
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-04-28 18:54:29 +0100

    AGAIN

--- shell.h
+++ shell.h
@@ -1,40 +1,13 @@
-#ifndef SHELL_H
-#define SHELL_H
+#include<unistd.h>
+#include<sys/wait.h>
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <signal.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-
-extern char **environ;
-
-/**
- * struct list_path -> Singly linked list
- * 
- * @path: Path
- * @ptr: Pointer Next Node
-*/
-
-typedef struct list_path
-{
-	char *path;
-	struct list_path *ptr;
-} list_path;
-
-/**
- * struct mybuild -> Struct of builtins
- * 
- * @name: Builtin Name
- * @func: Builtin Function
-*/
-
-typedef struct mybuild
-{
-	char *name;
-	void (*func)(char **);
-} mybuild;
-
-#endif
+#define LINE_LEN 80
+#define MAX_ARGS 64
+#define MAX_ARG_LEN 16
+#define MAX_PATHS 64
+#define MAX_PATH_LEN 96
+#define WHITESPACE " ,\t\n"
