6cb779376391008d413947f534d97c2dcad89d9f
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-04-28 16:44:57 +0100

    EveryThings  In One File

--- shell.c
+++ shell.c
@@ -1,49 +1,575 @@
 #include "shell.h"
 
 /**
- * main -> Entry point
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: 0
-*/
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _strlen -> The length of a string
+ *
+ * @s: Input
+ *
+ * Return: i String Length
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	return (i);
+}
+
+/**
+ * _puts -> Prints a string
+ *
+ * @str: Input
+ */
+
+void _putstr(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+}
+
+/**
+ * *_strdup -> Newly allocated space in memory
+ *
+ * @str: Input String
+ *
+ * Return: Depend Condition
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	int len;
+	char *new;
+
+	if (str == NULL)
+		return (NULL);
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	new = (char *) malloc(len + 1 * sizeof(char));
+	if (new == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+	{
+		new[i] = str[i];
+	}
+	new[i] = '\0';
+	return (new);
+}
+
+/**
+ * _atoi -> Convert string to integer
+ *
+ * @s: Input
+ *
+ * Return: Integer Converted
+ */
+
+int _atoi(char *s)
+{
+	int i;
+	int sin;
+	unsigned int digit;
+
+	i = 0;
+	sin = 1;
+	digit = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sin *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = (digit * 10) + (s[i] - '0');
+		}
+		else if (digit > 0)
+			break;
+		i++;
+	}
+	return (digit * sin);
+}
+
+void free_args(char **arv)
+{
+	int i;
+
+	for (i = 0; arv[i]; i++)
+		free(arv[i]);
+	free(arv);
+}
+
+void free_list(list_path *head)
+{
+	list_path *storage;
+
+	while (head)
+	{
+		storage = head->ptr;
+		free(head->path);
+		free(head);
+		head = storage;
+	}
+
+}
+
+void check_terminal(void)
+{
+	if (isatty(STDIN_FILENO))
+		_putstr("#cisfun$ ");
+}
+
+void _signal(int sig_num)
+{
+	if (sig_num == SIGINT)
+	{
+		_putstr("\n#cisfun$ ");
+	}
+}
+
+void _EOF(int len, char *buff)
+{
+	(void)buff;
+	if (len == -1)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			_putstr("\n");
+			free(buff);
+		}
+		exit(0);
+	}
+}
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *new;
+	char *old;
+
+	unsigned int i;
+
+	if (ptr == NULL)
+		return (malloc(new_size));
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	new = malloc(new_size);
+	old = ptr;
+	if (new == NULL)
+		return (NULL);
+
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			new[i] = old[i];
+		free(ptr);
+		for (i = old_size; i < new_size; i++)
+			new[i] = '\0';
+	}
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			new[i] = old[i];
+		free(ptr);
+	}
+	return (new);
+}
+
+void env_iron(char **arv __attribute__ ((unused)))
+{
+
+	int i;
+
+	for (i = 0; environ[i]; i++)
+	{
+		_putstr(environ[i]);
+		_putstr("\n");
+	}
+
+}
+
+char *get_env(const char *name)
+{
+	int i, j;
+	char *value;
+
+	if (!name)
+		return (NULL);
+	for (i = 0; environ[i]; i++)
+	{
+		j = 0;
+		if (name[j] == environ[i][j])
+		{
+			while (name[j])
+			{
+				if (name[j] != environ[i][j])
+					break;
+
+				j++;
+			}
+			if (name[j] == '\0')
+			{
+				value = (environ[i] + j + 1);
+				return (value);
+			}
+		}
+	}
+	return (0);
+}
+
+char *all_concat(char *name, char *sep, char *value)
+{
+	char *result;
+	int l1, l2, l3, i, k;
+
+	l1 = _strlen(name);
+	l2 = _strlen(sep);
+	l3 = _strlen(value);
+
+	result = malloc(l1 + l2 + l3 + 1);
+	if (!result)
+		return (NULL);
+
+	for (i = 0; name[i]; i++)
+		result[i] = name[i];
+	k = i;
+
+	for (i = 0; sep[i]; i++)
+		result[k + i] = sep[i];
+	k = k + i;
+
+	for (i = 0; value[i]; i++)
+		result[k + i] = value[i];
+	k = k + i;
+
+	result[k] = '\0';
+
+	return (result);
+}
+
+void set_env(char **arv)
+{
+	int i, j, k;
+
+	if (!arv[1] || !arv[2])
+	{
+		perror(get_env("_"));
+		return;
+	}
+
+	for (i = 0; environ[i]; i++)
+	{
+		j = 0;
+		if (arv[1][j] == environ[i][j])
+		{
+			while (arv[1][j])
+			{
+				if (arv[1][j] != environ[i][j])
+					break;
+
+				j++;
+			}
+			if (arv[1][j] == '\0')
+			{
+				k = 0;
+				while (arv[2][k])
+				{
+					environ[i][j + 1 + k] = arv[2][k];
+					k++;
+				}
+				environ[i][j + 1 + k] = '\0';
+				return;
+			}
+		}
+	}
+	if (!environ[i])
+	{
+
+		environ[i] = all_concat(arv[1], "=", arv[2]);
+		environ[i + 1] = '\0';
+
+	}
+}
+
+void unset_env(char **arv)
+{
+	int i, j;
+
+	if (!arv[1])
+	{
+		perror(get_env("_"));
+		return;
+	}
+	for (i = 0; environ[i]; i++)
+	{
+		j = 0;
+		if (arv[1][j] == environ[i][j])
+		{
+			while (arv[1][j])
+			{
+				if (arv[1][j] != environ[i][j])
+					break;
+
+				j++;
+			}
+			if (arv[1][j] == '\0')
+			{
+				free(environ[i]);
+				environ[i] = environ[i + 1];
+				while (environ[i])
+				{
+					environ[i] = environ[i + 1];
+					i++;
+				}
+				return;
+			}
+		}
+	}
+}
+
+void exit_line(char **arv)
+{
+	int i, n;
+
+	if (arv[1])
+	{
+		n = _atoi(arv[1]);
+		if (n <= -1)
+			n = 2;
+		free_args(arv);
+		exit(n);
+	}
+	for (i = 0; arv[i]; i++)
+		free(arv[i]);
+	free(arv);
+	exit(0);
+}
+
+void(*checkbuild(char **arv))(char **arv)
+{
+	int i, j;
+	mybuild T[] = {
+		{"exit", exit_line},
+		{"env", env_iron},
+		{"setenv", set_env},
+		{"unsetenv", unset_env},
+		{NULL, NULL}
+	};
+
+	for (i = 0; T[i].name; i++)
+	{
+		j = 0;
+		if (T[i].name[j] == arv[0][j])
+		{
+			for (j = 0; arv[0][j]; j++)
+			{
+				if (T[i].name[j] != arv[0][j])
+					break;
+			}
+			if (!arv[0][j])
+				return (T[i].func);
+		}
+	}
+	return (0);
+}
+
+char **split_line(char *str, const char *delim)
+{
+	int i, wn;
+	char **array;
+	char *token;
+	char *copy;
+
+	copy = malloc(_strlen(str) + 1);
+	if (copy == NULL)
+	{
+		perror(get_env("_"));
+		return (NULL);
+	}
+	i = 0;
+	while (str[i])
+	{
+		copy[i] = str[i];
+		i++;
+	}
+	copy[i] = '\0';
+
+	token = strtok(copy, delim);
+	array = malloc((sizeof(char *) * 2));
+	array[0] = _strdup(token);
+
+	i = 1;
+	wn = 3;
+	while (token)
+	{
+		token = strtok(NULL, delim);
+		array = _realloc(array, (sizeof(char *) * (wn - 1)), (sizeof(char *) * wn));
+		array[i] = _strdup(token);
+		i++;
+		wn++;
+	}
+	free(copy);
+	return (array);
+}
+
+void execute_command(char **argv)
+{
+
+	int d, status;
+
+	if (!argv || !argv[0])
+		return;
+	d = fork();
+	if (d == -1)
+	{
+		perror(get_env("_"));
+	}
+	if (d == 0)
+	{
+		execve(argv[0], argv, environ);
+			perror(argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	wait(&status);
+}
+
+list_path *end_node(list_path **head, char *str)
+{
+
+	list_path *tmp;
+	list_path *new;
+
+	new = malloc(sizeof(list_path));
+
+	if (!new || !str)
+	{
+		return (NULL);
+	}
+
+	new->path = str;
+
+	new->ptr = '\0';
+	if (!*head)
+	{
+		*head = new;
+	}
+	else
+	{
+		tmp = *head;
+
+		while (tmp->ptr)
+			tmp = tmp->ptr;
+		tmp->ptr = new;
+	}
+
+	return (*head);
+}
+
+list_path *linkpath(char *path)
+{
+	list_path *head = '\0';
+	char *token;
+	char *cpath = _strdup(path);
+
+	token = strtok(cpath, ":");
+	while (token)
+	{
+		head = end_node(&head, token);
+		token = strtok(NULL, ":");
+	}
+
+	return (head);
+}
+
+char *_which(char *filename, list_path *head)
+{
+	struct stat st;
+	char *string;
+
+	list_path *tmp = head;
+
+	while (tmp)
+	{
+
+		string = all_concat(tmp->path, "/", filename);
+		if (stat(string, &st) == 0)
+		{
+			return (string);
+		}
+		free(string);
+		tmp = tmp->ptr;
+	}
+
+	return (NULL);
+}
 
 int main(void)
 {
-	char *input = NULL, **args = NULL;
+	ssize_t len = 0;
+	char *buff = NULL, *value, *pathname, **arv;
 	size_t size = 0;
-	ssize_t in_len = 0;
-	struct stat st;
-	list_path *list = NULL;
+	list_path *head = '\0';
+	void (*f)(char **);
 
-	check_terminal();
-	signal(SIGINT, handle_signal);
-	while (1)
+	signal(SIGINT, _signal);
+	while (len != EOF)
 	{
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "$ ", 2);
-		in_len = get_input(&input, &size);
-		if (in_len == EOF)
-			handle_eof(in_len, input);
-		if (in_len == 1)
-		{
-			free(input);
-			continue;
-		}
-		args = split_input(input, " \n\t\r");
-		if (!args)
-		{
-			free(input);
-			continue;
-		}
-		if (stat(args[0], &st) == 0)
-			execute_command(args);
+		check_terminal();
+		len = getline(&buff, &size, stdin);
+		_EOF(len, buff);
+		arv = split_line(buff, " \n");
+		if (!arv || !arv[0])
+			execute_command(arv);
 		else
 		{
-			list = link_path(get_env("PATH"));
-			execute_command(args);
-			free_list(list);
+			value = get_env("PATH");
+			head = linkpath(value);
+			pathname = _which(arv[0], head);
+			f = checkbuild(arv);
+			if (f)
+			{
+				free(buff);
+				f(arv);
+			}
+			else if (!pathname)
+				execute_command(arv);
+			else if (pathname)
+			{
+				free(arv[0]);
+				arv[0] = pathname;
+				execute_command(arv);
+			}
 		}
-		free_args(args);
-		free(input);
 	}
+	free_list(head);
+	free_args(arv);
+	free(buff);
 	return (0);
 }
--- shell.h
+++ shell.h
@@ -11,54 +11,30 @@
 
 extern char **environ;
 
-char *_strcat(char *dest, char *src);
-int _strcmp(char *s1, char *s2);
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
-
 /**
- * list_path -> Struct To Store Path
- * @dir: Directory
- * @next: Pointer To Next Node
- */
+ * struct list_path -> Singly linked list
+ * 
+ * @path: Path
+ * @ptr: Pointer Next Node
+*/
 
 typedef struct list_path
 {
-    char *dir;
-    struct list_path *next;
+	char *path;
+	struct list_path *ptr;
 } list_path;
 
-void cd_func(char **args);
-void setenv_func(char **args);
-void unsetenv_func(char **args);
-void env_func(char **args);
-void exit_func(char **args);
-
-
 /**
- * builtin_t -> Struct To Store Builtins
- * @name: Name
- * @func: Function
- */
+ * struct mybuild -> Struct of builtins
+ * 
+ * @name: Builtin Name
+ * @func: Builtin Function
+*/
 
-typedef struct builtin
+typedef struct mybuild
 {
-    char *name;
-    void (*func)(char **);
-} builtin_t;
-
-void handle_signal(int sig);
-void check_terminal(void);
-ssize_t get_input(char **input, size_t *size);
-void handle_eof(ssize_t in_len, char *input);
-char **split_input(char *input, char *delim);
-char *get_env(char *var);
-list_path *link_path(char *path);
-char *find_command(char *command, list_path *list);
-void (*get_func(char **args))(char **);
-void execute_command(char **args);
-void free_list(list_path *list);
-void free_args(char **args);
+	char *name;
+	void (*func)(char **);
+} mybuild;
 
 #endif
